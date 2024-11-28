1f1d17bad5cfecceea76c8e61eceb68ae2551a36
Helen Yanushevskaya <qahelping@gmail.com>
2024-11-28 20:13:03 +0300

    add second tests

--- elements/header_element.py
+++ elements/header_element.py
@@ -1,3 +1,18 @@
+import allure
+from selenium.webdriver.common.by import By
 
-class HeaderElement():
-    pass
+from core.base import Base
+
+class HeaderElement(Base):
+    LOGO = (By.XPATH, '//*[@class="W3Prq tOEJx"]')
+
+    def __init__(self, driver):
+        super(HeaderElement, self).__init__(driver)
+        self.driver = driver
+
+    @allure.step('Assert that logo is visible')
+    def assert_logo_is_visible(self):
+        element = self.get_element(self.LOGO)
+        assert element.is_displayed(), f"Element {self.LOGO} is not visible"
+
+
--- tests/test_hotels.py
+++ tests/test_hotels.py
@@ -1,5 +1,7 @@
 import allure
+import pytest
 
+from elements.header_element import HeaderElement
 from pages.hotels_page import HotelsPage
 
 
@@ -10,3 +12,11 @@ class TestHotels:
         hotels_page = HotelsPage(driver)
         hotels_page.open()
         hotels_page.assert_page_is_opened()
+
+    @pytest.mark.only
+    def test_logo_is_visible(self, driver):
+        hotels_page = HotelsPage(driver)
+        hotels_page.open()
+
+        header_element = HeaderElement(driver)
+        header_element.assert_logo_is_visible()
